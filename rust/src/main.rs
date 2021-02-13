355fa916d5b146a34cfab4f6ffb74171dff35ae9
Shinichiro Kashiwagi <s.kashiwagi@gmail.com>
2021-02-13 15:32:09 +0800

    leetcode

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,19 @@
-mod netsted_list_weight_sum;
+fn main() {}
 
-fn main() {
-    netsted_list_weight_sum::Solution {};
+pub struct Solution {}
+
+impl Solution {
+    pub fn number_of_steps(num: i32) -> i32 {
+        0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(Solution::number_of_steps(14), 6);
+    }
 }

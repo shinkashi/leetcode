8bf65d8f304892210b931887e97b07787b421f0a
Shinichiro Kashiwagi <s.kashiwagi@gmail.com>
2021-05-30 16:49:34 +0800

    leetcode

---
+++ js/164.maximum-gap.ts
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode id=164 lang=typescript
+ *
+ * [164] Maximum Gap
+ *
+ * https://leetcode.com/problems/maximum-gap/description/
+ *
+ * algorithms
+ * Hard (37.18%)
+ * Likes:    1212
+ * Dislikes: 221
+ * Total Accepted:    104.5K
+ * Total Submissions: 277.9K
+ * Testcase Example:  '[3,6,9,1]'
+ *
+ * Given an integer array nums, return the maximum difference between two
+ * successive elements in its sorted form. If the array contains less than two
+ * elements, return 0.
+ * 
+ * You must write an algorithm that runs in linear time and uses linear extra
+ * space.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [3,6,9,1]
+ * Output: 3
+ * Explanation: The sorted form of the array is [1,3,6,9], either (3,6) or
+ * (6,9) has the maximum difference 3.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [10]
+ * Output: 0
+ * Explanation: The array contains less than 2 elements, therefore return
+ * 0.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 10^4
+ * 0 <= nums[i] <= 10^9
+ * 
+ * 
+ */
+
+// @lc code=start
+function maximumGap(nums: number[]): number {
+
+};
+// @lc code=end
+

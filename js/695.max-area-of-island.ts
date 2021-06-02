efcccc4c4319eb4eb28918e7dd6b915d5b813554
Shinichiro Kashiwagi <s.kashiwagi@gmail.com>
2021-06-02 13:24:40 +0800

    leetcode

---
+++ js/695.max-area-of-island.ts
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode id=695 lang=typescript
+ *
+ * [695] Max Area of Island
+ *
+ * https://leetcode.com/problems/max-area-of-island/description/
+ *
+ * algorithms
+ * Medium (65.27%)
+ * Likes:    3451
+ * Dislikes: 111
+ * Total Accepted:    269.1K
+ * Total Submissions: 404.9K
+ * Testcase Example:  '[[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]'
+ *
+ * You are given an m x n binary matrix grid. An island is a group of 1's
+ * (representing land) connected 4-directionally (horizontal or vertical.) You
+ * may assume all four edges of the grid are surrounded by water.
+ * 
+ * The area of an island is the number of cells with a value 1 in the island.
+ * 
+ * Return the maximum area of an island in grid. If there is no island, return
+ * 0.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: grid =
+ * [[0,0,1,0,0,0,0,1,0,0,0,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,1,1,0,1,0,0,0,0,0,0,0,0],[0,1,0,0,1,1,0,0,1,0,1,0,0],[0,1,0,0,1,1,0,0,1,1,1,0,0],[0,0,0,0,0,0,0,0,0,0,1,0,0],[0,0,0,0,0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,0,1,1,0,0,0,0]]
+ * Output: 6
+ * Explanation: The answer is not 11, because the island must be connected
+ * 4-directionally.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: grid = [[0,0,0,0,0,0,0,0]]
+ * Output: 0
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 50
+ * grid[i][j] is either 0 or 1.
+ * 
+ * 
+ */
+
+// @lc code=start
+function maxAreaOfIsland(grid: number[][]): number {
+    let maxCnt = 0;
+
+    const dfs = (y: number, x: number): number => {
+        if (grid[y][x] != 1) return 0;
+        let c = 1;
+        grid[y][x] = 2;
+        if (y > 0) c += dfs(y - 1, x);
+        if (x > 0) c += dfs(y, x - 1);
+        if (y < grid.length - 1) c += dfs(y + 1, x);
+        if (x < grid[0].length - 1) c += dfs(y, x + 1);
+        if (maxCnt < c) maxCnt = c;
+        return c;
+    }
+
+    for (let y = 0; y < grid.length; y++) {
+        for (let x = 0; x < grid[0].length; x++) {
+            if (grid[y][x] == 1) dfs(y, x);
+        }
+    }
+
+    // console.table(grid)
+
+    return maxCnt;
+};
+// @lc code=end
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+
+Deno.test("a", () => {
+    const grid = [[0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0], [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]]
+    assertEquals(maxAreaOfIsland(grid), 6)
+})
